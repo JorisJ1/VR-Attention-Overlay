1365d3dee998130c99cc6a95f5ccee521946aeed
Joris <jorisj@protonmail.com>
2021-01-29 17:08:01 +0100

    Works with absolute path

--- Start.h
+++ Start.h
@@ -1,58 +1,14 @@
 #include <stdio.h>
 #include <chrono>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <map>
 #include <openvr.h>
 #include <string>
-#include <sstream>
 #include <thread>
-#include <vector>
-#include <windows.h>
-
-// Return the directory containing the program executable.
-std::wstring GetExeDirectory() {
-	TCHAR buffer[MAX_PATH] = { 0 };
-	GetModuleFileName(NULL, buffer, MAX_PATH);
-	std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
-	return std::wstring(buffer).substr(0, pos);
-}
-
-// Convert a wstring to a string.
-std::string ConvertWstringToString(const std::wstring& wstr) {
-	int len = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, 0, 0, 0, 0);
-	std::vector<char> buf(len);
-	WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &buf[0], len, 0, 0);
-	return std::string(buf.begin(), buf.end());
-}
-
-// Check whether a file exists.
-bool IsExistingFile(const std::string& name) {
-	struct stat buffer;
-	return (stat(name.c_str(), &buffer) == 0);
-}
 
 // Start function.
 int main(int argc, char** argv) {
 
-	// Check whether an filename has been provided and if the file exists.
-	std::string argument;
-	if (argc <= 1) {
-		/*std::cout << "No argument had been passed. Please pass the filename of the desired image as an argument.\n";
-		return 0;*/
-		argument = "attention.png";
-	} else {
-		argument = argv[1];
-	}
-
-	std::string imagePath = ConvertWstringToString(GetExeDirectory()) + '\\' + argument;
-	if (!std::filesystem::exists(imagePath)) {
-		std::cout << "File not found: " << imagePath << '\n';
-		return 0;
-	} else {
-		std::cout << "File present: " << imagePath << '\n';
-	}
+	std::string imagePath = argv[1];
+	const char* imagePathArr = imagePath.c_str();
 
 	// Initialise VR.
 	vr::EVRInitError error;
@@ -61,8 +17,6 @@ int main(int argc, char** argv) {
 	if (error != 0) {
 		printf("%d: error %s\n", __LINE__, VR_GetVRInitErrorAsSymbol(error));
 		return 0;
-	} else {
-		std::cout << "No VR errors: " << '\n';
 	}
 
 	// Specify the location of the image in VR. 
@@ -84,13 +38,11 @@ int main(int argc, char** argv) {
 	// Initialise and show the VR overlay.
 	vr::VROverlayHandle_t handle;
 	vr::VROverlay()->CreateOverlay("imgOverlayKey1", "imgOverlayName1", &handle);
-	vr::VROverlay()->SetOverlayFromFile(handle, imagePath.c_str());
+	vr::VROverlay()->SetOverlayFromFile(handle, imagePathArr);
 	//vr::VROverlay()->SetOverlayFromFile(handle, "attention.png");
 	vr::VROverlay()->SetOverlayWidthInMeters(handle, 1);
 	vr::VROverlay()->ShowOverlay(handle);
 	vr::VROverlay()->SetOverlayTransformTrackedDeviceRelative(handle, vr::k_unTrackedDeviceIndex_Hmd, &transform);
-
-	std::cout << "Sleeping..." << '\n';
 
 	// Sleep, to keep the overlay on screen.
 	using namespace std::chrono_literals;
